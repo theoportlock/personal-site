7f5292a8b068e3d4b8c3c81cbe4e5e98ee24ca5a
theoportlock <theo.portlock@auckland.ac.nz>
2026-03-12 16:40:13 +1300

    Update projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,21 +1,21 @@
 const data = [
   {
-    title: 'Tis',
-    subtitle: 'Pattern recognition of bitarray datastreams',
-    link: 'https://github.com/theoportlock/tis.git',
-    image: '/images/projects/combination.jpg',
-    date: '2012-11-20',
-    desc:
-      'Looks at hypercombinations of bitarrays for prediction.',
-  },
-  {
     title: 'Workforce',
     subtitle: 'Scheduler for bash commands',
     link: 'https://pypi.org/project/workforce/',
     image: '/images/projects/example_plan.jpg',
-    date: '2020-09-20',
+    date: '2026-03-12',
     desc:
       'Uses python networkx and multiprocessing to run a csv edgelist of bash commands parallel',
+  },
+  {
+    title: 'Tis',
+    subtitle: 'Pattern recognition of bitarray datastreams',
+    link: 'https://github.com/theoportlock/tis.git',
+    image: '/images/projects/arch.png',
+    date: '2012-11-20',
+    desc:
+      'Looks at hypercombinations of bitarrays for prediction.',
   },
 ];
 
